7c09cad3723ff1cb484326d0c5f40501fa23944d
karanOO89 <gk.987680@gmail.com>
2021-04-07 21:04:50 +0000

    snake client added

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost' ,
+    port: 50541
+    
+    
+});
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', function(message) {
+      console.log(`Recieved Packets:${message}`)
+  })
+  return conn;
+}
+console.log('Connecting ...');
+connect();
