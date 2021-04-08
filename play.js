718b649f46d2fb91a262dedb2fa0c753cd64a6e5
karanOO89 <gk.987680@gmail.com>
2021-04-08 01:59:50 +0000

    adding stdin

--- play.js
+++ play.js
@@ -4,11 +4,28 @@
 
 const connect = require("./client");
 const conn = connect();
+const handleUserInput = (key) => {
+  console.log("no idea",key)
+    if (key === '\u0003') {
+        process.exit();
+      }
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data',(key) => handleUserInput(key))
+  return stdin;
+};
+
 console.log("Connecting ...");
+
 conn.on("connect", function () {
+  
   console.log(`Connection Established`);
   conn.write("Name: KG");
+  setupInput(conn);
+ 
 });
 
-
-// conn.setEncoding("utf8");
