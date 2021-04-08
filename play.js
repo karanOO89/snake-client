8bcb51d8dac16806175ec7d3e0177244c22c9be9
karanOO89 <gk.987680@gmail.com>
2021-04-08 02:38:19 +0000

    wasd added

--- play.js
+++ play.js
@@ -2,30 +2,10 @@
  * Establishes connection with the game server
  */
 
-const connect = require("./client");
-const conn = connect();
-const handleUserInput = (key) => {
-  console.log("no idea",key)
-    if (key === '\u0003') {
-        process.exit();
-      }
-}
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data',(key) => handleUserInput(key))
-  return stdin;
-};
-
-console.log("Connecting ...");
-
-conn.on("connect", function () {
-  
-  console.log(`Connection Established`);
-  conn.write("Name: KG");
-  setupInput(conn);
+ const connect  = require('./client');
+ const  setupInput  = require('./input');
  
-});
-
+ console.log('Connecting ...');
+ const conn = connect();
+ 
+ setupInput(conn);
